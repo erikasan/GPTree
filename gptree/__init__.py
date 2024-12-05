b3007fc52cc073c67d6e232c48a4a0efe2783a4a
Erik Alexander Sandvik <e.a.sandvik@fys.uio.no>
2024-12-05 18:31:14 +0100

    Removed GPForest

--- gptree/__init__.py
+++ gptree/__init__.py
@@ -6,7 +6,7 @@ from sklearn.utils import resample
 
 from numpy.random import binomial
 
-from typing import Callable, Optional, Type, Union
+from typing import Callable, Optional, Type
 
 from copy import deepcopy
 
@@ -46,7 +46,6 @@ class GPNode(Node):
         self.parent = None
         self.children = None
         
-        self.is_root = False
         self.is_left = None
         self.is_leaf = False
         
@@ -114,9 +113,11 @@ class GPNode(Node):
         """ Fit the GP of the node with sklearn. """
         self.my_GPR.fit(self.my_X_data, self.my_y_data)
 
+
+
     def compute_s_and_o(self, theta: float): ## TODO: This needs a better name lmao
         """ Find the position s of the dividing hyperplane and the overlapping region o. """
-        w = np.empty(self.n_features)
+        w = np.zeros(self.n_features)
         for i in range(self.n_features):
             w[i] = np.max(self.my_X_data[:, i]) - np.min(self.my_X_data[:, i])
         self.j = np.argmax(w)
@@ -125,13 +126,7 @@ class GPNode(Node):
         self.o = theta*w[self.j]
 
     def prob_func(self, x: np.array):
-        """ The default probability function as suggested in the DLGP article.
-            The probability that a point (x, y) is sent to the right child node. """
-        
-
-        # TEMPORARY
-        #self.s = 99.0
-
+        """ The default probability function as suggested in the DLGP article. """
         prob = (x[:, self.j] - self.s)/self.o + 0.5
         prob[prob < 0] = 0
         prob[prob > 1] = 1
@@ -182,7 +177,6 @@ class GPTree:
         
         self.GPR = GPR
         self.root = GPNode(0, my_GPR=GPR)  # Initialize root node of the GPTree
-        self.root.is_root = True
         
         self.Nbar = Nbar
         self.theta = theta
@@ -195,6 +189,7 @@ class GPTree:
         node = self.root  # Start from root node
         while node.children:  # Then follow a random branch until a leaf node is reached
             node = node.children[int(binomial(1, node.prob_func(x)))]
+        
 
         if node.num_training_points == self.Nbar:   # If the node is full
             node.generate_children(self.GPR, self.n_features) # generate child nodes
@@ -261,7 +256,7 @@ class GPTree:
                 
     
     
-    def predict(self, X_test: np.ndarray, recursive_search: Optional[bool]=True, show_progress: Optional[bool]=False):
+    def predict(self, X_test: np.ndarray, show_progress: Optional[bool]=False):
         """ 
         Predict the target function at selected points in feature space. 
         
@@ -271,14 +266,6 @@ class GPTree:
         X_test: np.ndarray
             The points in feature space where we'd like to predict the target function. Has shape=(N_test, n_features).
 
-        recursive_search: Optional[bool]=True
-            Prediction with recursive search of leafs with nonzero marginal probabilities.
-            The method recursive_predict() is called instead.
-
-            If false the predictions of leafs with zero marginal probabilities are also computed. 
-
-            recursive_search should be set to True when N >> Nbar. Otherwise, this approach is actually slower. 
-
         show_progress: Optional[bool]=False
             Display a progress bar in the terminal using tqdm.
 
@@ -291,36 +278,32 @@ class GPTree:
         std_DLGP: np.ndarray
             The posterior standard deviation used to quantify the uncertainty in the prediction. Has shape=(N_test, 1).
         """
+        mean_DLGP = np.zeros((X_test.shape[0], 1))
+        var_DLGP = np.zeros((X_test.shape[0], 1))
 
-        if recursive_search: # Recursive search
-            mean_DLGP, std_DLGP = self.recursive_predict(X_test, show_progress)
-        
-        else:   # Loop over all leaf nodes, even those with zero weights. 
-            mean_DLGP = np.zeros((X_test.shape[0], 1))
-            var_DLGP = np.zeros((X_test.shape[0], 1))
-
-            for leaf in tqdm(self.root.leaves, disable=not show_progress, desc="Predicting"):
-                
-                mu_leaf, sigma_leaf = leaf.my_GPR.predict(X_test, return_std=True)
-                mu_leaf = mu_leaf.reshape(mean_DLGP.shape)
-                sigma_leaf = sigma_leaf.reshape(mean_DLGP.shape)
-
-                ptilde = leaf.marg_prob(X_test)
-                ptilde = ptilde.reshape(mean_DLGP.shape)
-
-                mean_DLGP += ptilde*mu_leaf
-
-                var_DLGP += ptilde*(sigma_leaf*sigma_leaf + mu_leaf*mu_leaf)
+        for leaf in tqdm(self.root.leaves, disable=not show_progress, desc="Predicting"):
             
-            var_DLGP += -mean_DLGP*mean_DLGP
-            std_DLGP = np.sqrt(var_DLGP)
+            #leaf.compute_my_GPR()
+            mu_leaf, sigma_leaf = leaf.my_GPR.predict(X_test, return_std=True)
+            mu_leaf = mu_leaf.reshape(mean_DLGP.shape)
+            sigma_leaf = sigma_leaf.reshape(mean_DLGP.shape)
 
-        return mean_DLGP, std_DLGP
+            ptilde = leaf.marg_prob(X_test)
+            ptilde = ptilde.reshape(mean_DLGP.shape)
+
+            mean_DLGP += ptilde*mu_leaf
+
+            var_DLGP += ptilde*(sigma_leaf*sigma_leaf + mu_leaf*mu_leaf)
+        
+        var_DLGP += -mean_DLGP*mean_DLGP
+
+        return mean_DLGP, np.sqrt(var_DLGP)
     
 
-    def recursive_predict(self, X_test: np.ndarray, show_progress: Optional[bool]=False):
+    def _predict(self, X_test: np.ndarray, show_progress: Optional[bool]=False):
         """ 
-        Second predict function with recursive search of leaves with nonzero marginal probabilities.  
+        Second predict function using recursive function to collect contributing leaves.  
+        Will ̶m̶a̶y̶b̶e̶  likely replace the other one when N_train >> Nbar. 
         
         Arguments
         ---------
@@ -341,20 +324,15 @@ class GPTree:
             The posterior standard deviation used to quantify the uncertainty in the prediction. Has shape=(N_test, 1).
         """
         
-
-        # Unneccesary to keep track of whether the marginal probabilities of the currently collected leaf nodes add up to one.
-        #global sum_probs, collection_done
+        global sum_probs, collection_done
 
         
         def collect_leaves(x: np.ndarray, current_node: GPNode, current_prob: float):
             """ Recursive function to collect contributing leaves for prediction at a test point x.  """
 
-            #global sum_probs, collection_done
+            global sum_probs, collection_done
 
-            """ if collection_done or current_prob <= 0:
-                return """
-            
-            if current_prob <= 0:
+            if collection_done or current_prob <= 0:
                 return
             
             # Return if we have reached a leaf node
@@ -362,9 +340,9 @@ class GPTree:
                 leaves.append(current_node)
                 pred_leaf_probs.append(current_prob)
 
-                """ sum_probs += current_prob
+                sum_probs += current_prob
                 if sum_probs >= 1:
-                    collection_done = True   """             
+                    collection_done = True                
                 return
 
             # Ok, not a leaf node. Now, for both child nodes:
@@ -389,113 +367,13 @@ class GPTree:
         for i, x in tqdm(enumerate(X_test), total=X_test.shape[0], disable=not show_progress, desc="Predicting"):
             x = x.reshape((1, x.shape[0]))
 
-            """ sum_probs = 0
-            collection_done = False """
+            sum_probs = 0
+            collection_done = False
 
             leaves = []
             pred_leaf_probs = []
 
             collect_leaves(x, self.root, 1)
-        
-            for leaf, ptilde in zip(leaves, pred_leaf_probs):
-                
-                mu_leaf, sigma_leaf = leaf.my_GPR.predict(x, return_std=True)
-
-                # This is the stupid part
-                mean_DLGP[i] += ptilde[0]*mu_leaf[0]
-                var_DLGP[i] += ptilde[0]*(sigma_leaf[0]*sigma_leaf[0] + mu_leaf[0]*mu_leaf[0])
-            
-            var_DLGP[i] += -mean_DLGP[i]*mean_DLGP[i]
-        
-        return mean_DLGP, np.sqrt(var_DLGP)
-    
-
-    def recursive_predict_algortihm_2(self, X_test):
-        """ Third predict function if you can believe it.
-         
-            Literal implementation of Algorithm 2 in Gaussian Process-Based Real-Time Learning for Safety Critical Applications by Lederer et al.
-            Seems to be somewhat slower than recursive_predict when N >> N_bar, so recursive_predict is preferred.
-        """
-
-        def ____predict(self, x: np.ndarray, n: GPNode):
-            if n.is_leaf:
-                mu_n, sigma_n = n.my_GPR.predict(x, return_std=True)
-                return [mu_n], [sigma_n*sigma_n], [1]
-            
-            else:
-                mu_list = []; var_list = []; omega_list = []
-
-                p = n.prob_func(x)
-                if  p > 0:
-                    mu_hat, var_hat, omega_hat = ____predict(self, x, n.right)
-                    mu_list = mu_list + mu_hat; var_list = var_list + var_hat;
-                    
-                    omega_list = omega_list + [omega*p for omega in omega_hat]
-                if 1 - p > 0:
-                    mu_hat, var_hat, omega_hat = ____predict(self, x, n.left)
-                    mu_list = mu_list + mu_hat; var_list = var_list + var_hat;
-                    
-                    omega_list = omega_list + [omega*(1-p) for omega in omega_hat]
-
-                if not n.is_root:
-                    return mu_list, var_list, omega_list
-                
-                else:
-                    mu_DLGP = 0
-                    var_DLGP = 0
-
-                    for mu, var, omega in zip(mu_list, var_list, omega_list):
-                        mu_DLGP += mu[0]*omega[0]
-
-                        var_DLGP += (mu[0]*mu[0] + var[0])*omega[0]
-                    
-                    var_DLGP -= mu_DLGP[0]*mu_DLGP[0]
-
-                    return mu_DLGP, np.sqrt(var_DLGP)
-
-        mean_DLGP = np.zeros((X_test.shape[0], 1))
-        std_DLGP = np.zeros((X_test.shape[0], 1))
-
-        for i, x in enumerate(X_test):
-            x = x.reshape((1, x.shape[0]))
-            mean_DLGP[i], std_DLGP[i] = ____predict(self, x, self.root)
-        
-        return mean_DLGP, std_DLGP
-    
-
-
-    def recursive_predict_algortihm_3(self, X_test, show_progress=True):
-        """ Third predict function if you can believe it.
-         
-            Implementation of Algorithm 2 in Gaussian Process-Based Real-Time Learning for Safety Critical Applications by Lederer et al.
-            Seems to be somewhat slower than recursive_predict when N >> N_bar, so recursive_predict is preferred.
-        """
-
-        def _collect_leaves(x: np.ndarray, n: GPNode):
-            if n.is_leaf:
-                return [n], [1]
-            
-            else:
-                leaves = []; omega_list = []
-
-                p = n.prob_func(x)
-                if  p > 0:
-                    ell_hat,  omega_hat = _collect_leaves(x, n.right)
-                    leaves = leaves + ell_hat; omega_list = omega_list + [omega*p for omega in omega_hat]
-                    
-                if 1 - p > 0:
-                    ell_hat,  omega_hat = _collect_leaves(x, n.left)
-                    leaves = leaves + ell_hat; omega_list = omega_list + [omega*(1-p) for omega in omega_hat]
-
-            return leaves, omega_list
-        
-
-        mean_DLGP = np.zeros((X_test.shape[0], 1))
-        var_DLGP = np.zeros((X_test.shape[0], 1))
-        for i, x in tqdm(enumerate(X_test), total=X_test.shape[0], disable=not show_progress, desc="Predicting"):
-            x = x.reshape((1, x.shape[0]))
-
-            leaves, pred_leaf_probs = _collect_leaves(x, self.root)
         
             for leaf, ptilde in zip(leaves, pred_leaf_probs):
                 
@@ -519,85 +397,3 @@ class GPTree:
     
     def save(self, path: str):
         joblib.dump(self, path)
-
-
-
-class GPForest:
-    def __init__(self,
-                 GPR: Optional[Union[GaussianProcessRegressor, list]] = Default_GPR(),
-                 Nbar: Optional[Union[int, list]] = 100,
-                 theta: Optional[Union[float, list]] = 0.0001):
-        
-        self.GPR = GPR
-        self.Nbar = Nbar
-        self.theta = theta
-
-        self.GPTrees = []
-
-        
-        if type(self.Nbar) == int:
-            self.Nbar = [self.Nbar]
-
-        if type(self.theta) == int:
-            self.theta = [self.theta]
-        
-        assert len(self.Nbar) == len(self.theta), "Nbar and theta must have equally many elements"
-
-        self.num_GPTrees = len(self.Nbar)
-
-        if type(self.GPR) == GaussianProcessRegressor or type(self.GPR) == Default_GPR:
-            self.GPR = self.num_GPTrees*[self.GPR]
-
-
-    def fit(self, X_train: np.ndarray, y_train: np.ndarray, show_progress: Optional[bool]=False):
-
-        for i in tqdm(range(self.num_GPTrees), disable=not show_progress, desc='Building forest'):
-            self.GPTrees.append(GPTree(self.GPR[i], self.Nbar[i], self.theta[i]))
-        
-        for i in tqdm(range(self.num_GPTrees), disable=not show_progress, desc='Training GPTrees'):
-            self.GPTrees[i].fit(X_train, y_train, shuffle=True)
-
-    def predict(self, X_test: np.ndarray, show_progress: Optional[bool]=False):
-        mean = np.zeros((X_test.shape[0], 1))
-        std = np.zeros((X_test.shape[0], 1))
-
-        mean_list = []
-        std_list = []
-
-        alpha = []
-        T = []
-
-        for i in tqdm(range(self.num_GPTrees), disable=not show_progress, desc='Predicting'):
-            mean_i, sigma_i = self.GPTrees[i].predict(X_test)
-
-            mean_list.append(mean_i)
-            std_list.append(sigma_i)
-
-            T.append(1./(sigma_i*sigma_i))
-
-            sigma_prior = np.diag(self.GPTrees[i].GPR.kernel(X_test)).reshape(-1, 1)
-
-            alpha.append(0.5*(np.log(sigma_prior) - np.log(sigma_i)))
-
-        sum_alpha = np.sum(alpha)
-        
-        for i in range(self.num_GPTrees):
-
-            mean += mean_list[i]*alpha[i]*T[i]/sum_alpha
-
-            std += alpha[i]*T[i]/sum_alpha
-            
-            
-        std = 1./std
-        
-        mean *= std
-
-        return mean, std
-
-    def save(self, path: str):
-        joblib.dump(self, path)
-
-
-        
-        
-
